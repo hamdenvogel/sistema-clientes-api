5d7c4f6158415bc804c8c6ea87be31bbe1feee66
Hamden Vogel <hamdenvogel@gmail.com>
2025-12-14 19:08:06 -0300

    First Deploy - part II
    First Deploy - part II

--- clientes/get_sonar_issues.ps1
+++
@@ -1,3 +0,0 @@
-$headers = @{ Authorization = "Basic " + [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes("sqp_ae579a1a6cf6638dcf1ab52a27a234f5adc74956:")) }
-$response = Invoke-RestMethod -Uri "http://localhost:9000/api/issues/search?componentKeys=clientes&resolved=false&severities=MAJOR,CRITICAL" -Headers $headers
-$response.issues | Select-Object component, message, line, severity | ConvertTo-Json
